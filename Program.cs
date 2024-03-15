2b06da7b22929781143be06694f4075ee0447bd7
kadumenezes <kadumenezes08@gmail.com>
2024-03-14 22:00:40 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+       
+            
+                Console.WriteLine();
+            Console.WriteLine("digite produto add estoque");
+            int qte = int.Parse(Console.ReadLine());
+             p.AdicionarProdutos(qte);
+            
+            Console.WriteLine("Dados do produto: " + p);
+           
+             
+             Console.WriteLine("digite produto remove estoque");
+              qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte); 
+            Console.WriteLine("Dados do produto: " + p);
+            
+            
+        }
+    }
+}
